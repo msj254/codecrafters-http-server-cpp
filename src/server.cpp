c34fbf44ee59df7c81f1decb3089fa15db19b85b
msj254 <hoodedrain@gmail.com>
2024-07-30 15:21:06 -0700

    adding user agent function 11

--- src/server.cpp
+++ src/server.cpp
@@ -103,8 +103,8 @@ int main(int argc, char **argv) {
   else if (client_message.starts_with("GET /user-agent"))
   {
     int found = client_message.find("User-Agent:");
-    int accept_found = client_message.find("");
-    //accept_found = found + accept_found;
+    int accept_found = client_message.substr(found+12).find(" ");
+    accept_found = found + accept_found + 12;
     std::string usr_msg = client_message.substr(found+12,accept_found);
     std::stringstream usr_msg_size;
     usr_msg_size << usr_msg.size();
