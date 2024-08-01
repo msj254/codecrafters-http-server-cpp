82b9a632cfbe6489c321c900a9f9b20373880a3b
msj254 <hoodedrain@gmail.com>
2024-07-31 23:03:06 -0700

    implementing encoding feature lite 7

--- src/server.cpp
+++ src/server.cpp
@@ -37,12 +37,13 @@ int handle_request(int client_fd, struct sockaddr_in client_addr, std::string di
    std::stringstream echo_msg_size;
    echo_msg_size << echo_msg.size();
 
-    std::cout << "ENCODING: " << client_message.find("gzip") << std::endl;
-    std::cout << "INVALID: " << client_message.find("invalid") << std::endl;
-    std::cout << "SIZE: " << client_message.size() << std::endl;
-   if (client_message.find("Accept-Encoding")!=client_message.size())
+    //std::cout << "ENCODING: " << client_message.find("gzip") << std::endl;
+    //std::cout << "INVALID: " << client_message.find("invalid") << std::endl;
+    //std::cout << "SIZE: " << client_message.size() << std::endl;
+    
+   if (client_message.find("Accept-Encoding")!=std::string::npos)
    {
-      if (client_message.find("gzip")!=client_message.size()) 
+      if (client_message.find("gzip")!=std::string::npos) 
           {response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n\r\n";}
 
       else {response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";}
